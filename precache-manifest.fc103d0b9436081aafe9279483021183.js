c4d26a3286aaafd9da20c5ea64ef66c17fb5ea90
iamadim <adim4usall@gmail.com>
2020-02-17 22:29:15 -0500

    Updates

---
+++ precache-manifest.fc103d0b9436081aafe9279483021183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4094f600dae885b7b3464ce573e660d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f40afa3675aa47f862dc",
+    "url": "/monster-rolodex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04f5747e520d0ed63807",
+    "url": "/monster-rolodex/static/js/2.fdd34e15.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.fdd34e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40afa3675aa47f862dc",
+    "url": "/monster-rolodex/static/js/main.73ca3b1c.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
